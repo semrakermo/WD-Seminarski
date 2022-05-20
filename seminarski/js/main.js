2139c82e4f7aa2efcf30c4055fb1268f3cbc5d27
semrakermo <96873916+semrakermo@users.noreply.github.com>
2022-05-20 21:02:48 +0200

    Delete
    Uradjen delete, css nije jos skroz namjesten

--- seminarski/js/main.js
+++ seminarski/js/main.js
@@ -9,6 +9,8 @@ const processData = data => {
     const mainDiv = document.getElementById('parent');
     const row = document.getElementById('row');
 
+    const dropdown = document.getElementById('dropdown');
+
     data.forEach(element => {
 
         const div = document.createElement('div');
@@ -18,19 +20,27 @@ const processData = data => {
         img.classList.add('feature-images');
         img.src = element.imageUrl;
 
-      const h3 = document.createElement('h3');
-      h3.classList.add('feature-title');
-      h3.innerHTML = element.name;
+        const h3 = document.createElement('h3');
+        h3.classList.add('feature-title');
+        h3.innerHTML = element.name;
 
-      const p = document.createElement('p');
-      p.innerHTML = element.price + " KM";
+        const p = document.createElement('p');
+        p.innerHTML = element.price + " KM";
 
-      div.appendChild(img);
-      div.appendChild(h3);
-      div.appendChild(p);
-      row.appendChild(div);
-      mainDiv.appendChild(row);
-      section.appendChild(mainDiv);
+        div.appendChild(img);
+        div.appendChild(h3);
+        div.appendChild(p);
+        row.appendChild(div);
+        mainDiv.appendChild(row);
+        section.appendChild(mainDiv);
+
+
+        const option = document.createElement('option');
+        option.innerHTML = element.id + ". " + element.name;
+        option.setAttribute("value", element.id);
+        option.classList.add('dropdown-item');
+
+        dropdown.appendChild(option);
     });
 }
 
@@ -43,7 +53,7 @@ forma.addEventListener('submit', function(e){
     let price = document.getElementById('price');
     let imageUrl = document.getElementById('imageUrl');
     
-    fetch('https://reqres.in/api/users', {
+    fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food', {
         method: 'POST',
         headers:{
             "Content-type":"application/json"
@@ -54,6 +64,26 @@ forma.addEventListener('submit', function(e){
             "imageUrl": imageUrl.value,
         })
     })
-        .then(res => res.json())
         .then(data => console.log(data))
 })
+
+
+//Funkcija koja uzima id hrane iz dropdown menija
+
+function getOption() {
+    selectElement = document.querySelector('#dropdown');
+    id = selectElement.value;
+    document.querySelector('.output').textContent = "Option " + id + " has been successfully deleted!";
+    deleteFood(id);
+}
+
+
+//Delete
+const deleteFood = (id) => {
+    fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food/${id}`, {
+        method: "DELETE",
+    })
+    .then((res) => {
+        console.log(res);
+    })
+}
