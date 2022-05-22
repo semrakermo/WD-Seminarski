014fef6dc0cd1b05e1ff06801ded157385f3bf63
semrakermo <96873916+semrakermo@users.noreply.github.com>
2022-05-22 15:20:23 +0200

    Minor prepravke
    Uljepsan kod, dodano par komentara, izbrisan css koji je bio viska

--- seminarski/js/main.js
+++ seminarski/js/main.js
@@ -3,9 +3,7 @@ fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
     .then(response => response.json())
     .then(data => processData(data));
 
-const processData = data => {
-    
-
+const processData = data => { //Render hrane na stranici
 
     const section = document.getElementById('sec');
     const mainDiv = document.getElementById('parent');
@@ -40,6 +38,10 @@ const processData = data => {
         button.setAttribute("data-price", element.price);
         button.innerHTML = "Edit";
 
+        const option = document.createElement('option');
+        option.innerHTML = element.id + ". " + element.name;
+        option.setAttribute("value", element.id);
+        option.classList.add('dropdown-item');
 
         div.appendChild(img);
         div.appendChild(h3);
@@ -48,37 +50,33 @@ const processData = data => {
         row.appendChild(div);
         mainDiv.appendChild(row);
         section.appendChild(mainDiv);
-
-
-        const option = document.createElement('option');
-        option.innerHTML = element.id + ". " + element.name;
-        option.setAttribute("value", element.id);
-        option.classList.add('dropdown-item');
-
         dropdown.appendChild(option);
     });
 }
 
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var putId = button.data('id') // Extract info from data-* attributes
-  var putName = button.data('name')
-  var putImg = button.data('image')
-  var putPrice = button.data('price')
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('edit food')
-  modal.find('.modal-body #foodID').val(putId)
-  modal.find('.modal-body #recipient-name').val(putName)
-  modal.find('.modal-body #foodImg').val(putImg)
-  modal.find('.modal-body #price-text').val(putPrice)
+//Funkcija koja ispisuje trenutke podatke o foodu u modal
+$('#exampleModal').on('show.bs.modal', function (event) { 
+
+  var button = $(event.relatedTarget); // Button koji je trigerovo modal
+
+  var putId = button.data('id'); //Uzimanje trenutnih podatka o tom foodu
+  var putName = button.data('name');
+  var putImg = button.data('image');
+  var putPrice = button.data('price');
+
+  var modal = $(this);
+  modal.find('.modal-title').text('edit food'); //Ispisivanje trenutnih podataka o foodu u modal
+  modal.find('.modal-body #foodID').val(putId);
+  modal.find('.modal-body #recipient-name').val(putName);
+  modal.find('.modal-body #foodImg').val(putImg);
+  modal.find('.modal-body #price-text').val(putPrice);
 })
 
 
 //Post
 let forma = document.getElementById('forma');
 forma.addEventListener('submit', function(e){
+    console.log('POST')
     e.preventDefault();
 
     let name = document.getElementById('name');
@@ -110,6 +108,7 @@ function getOption() {
 
 //Delete
 const deleteFood = (id) => {
+    console.log('DELETE');
     fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food/${id}`, {
         method: "DELETE",
     })
@@ -125,6 +124,8 @@ const updateFood = () =>{
     const foodName = document.getElementById('recipient-name').value;
     const foodImg = document.getElementById('foodImg').value;
     const foodPrice = document.getElementById('price-text').value;
+
+    console.log('PUT');
 
     fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food',{
         method:"PUT",
